29930cf33d3f8a58bc655222c3b6b89348a84c26
paschoalAr <arthur.paschoal@edu.pucrs.br>
2025-12-29 17:48:03 -0300

    add

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,19 @@
-const CACHE_NAME = 'srb-carteira-v1';
+const CACHE_NAME = 'srb-v2';
 const ASSETS = [
   './',
   './index.html',
+  './style.css',
   './carteira.png',
   './logo.png',
+  './comunicado.png',
+  './perfil.jpg',
   './manifest.json'
 ];
 
-// Instalação: Salva os arquivos no cache
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(ASSETS);
-      })
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-// Ativação: Limpa caches antigos
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Estratégia: Tenta o Cache primeiro, se não tiver (ou for primeiro acesso), vai na rede
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
